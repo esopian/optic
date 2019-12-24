a9a267a865d71868a1d000161c2e782075029417
Dev Doshi <dev@useoptic.com>
2019-12-23 19:28:33 -0500

    newline escape character will not be interpreted by bash so it should be removed

--- domain/scripts/package.sh
+++ domain/scripts/package.sh
@@ -6,8 +6,8 @@ API_CLI_FILE=../api-cli/provided/domain.js
 mkdir -p `dirname $OUTPUT_FILE`
 mkdir -p `dirname $API_CLI_FILE`
 
-echo "/* eslint-disable */\n" > $OUTPUT_FILE
-echo "/* eslint-disable */\n" > $API_CLI_FILE
+echo "/* eslint-disable */" > $OUTPUT_FILE
+echo "/* eslint-disable */" > $API_CLI_FILE
 cat target/scala-2.12/seamless-ddd-fastopt.js >> $OUTPUT_FILE
 cat target/scala-2.12/seamless-ddd-fastopt.js >> $API_CLI_FILE
 
